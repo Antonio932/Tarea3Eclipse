4adaa60c6e84d096640d732b2d330faa012e2f7f
Antonio Jimenez <ajimmon2207@g.educaand.es>
2025-12-16 13:33:18 +0100

    Añado cambios al programa2 rama2

--- src/Programas/programa2.java
+++ src/Programas/programa2.java
@@ -2,10 +2,9 @@ package Programas;
 import java.util.Scanner;
 
 public class programa2 {
-	public class Enteros {
-	    public static void funcion() {
+	 public static void funcion() {
 	        Scanner sc = new Scanner(System.in);
-	        int min, max;
+	        int min, max,tercero;
 
 	        do {
 	            System.out.print("Ingrese el valor minimo: ");
@@ -15,16 +14,21 @@ public class programa2 {
 	            max = sc.nextInt();
 
 	        } while (min == max || min > max);
+	        System.out.println("Ingrese el tercer número");
+	        tercero = sc.nextInt();
 
 	        int suma = min;
 
 	        while (suma <= max) {
 	            System.out.println((suma = suma + 7));
+
+	        }
+	        if (tercero >= min && tercero <= max) {
+	            System.out.println("Tercero se encuentra entre el min y el max");
 	        }
 	    }
 
 	    public static void main(String[] args) {
 	        funcion();
 	    }
-}
 }
